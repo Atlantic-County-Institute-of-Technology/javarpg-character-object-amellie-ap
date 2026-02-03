35bae19228499dc1e361d07089da2eaa79c4b1e6
ariz9258 <127510476+amellie-ap@users.noreply.github.com>
2026-02-03 12:57:02 -0500

    Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         Character evil = new Character();
         evil.createCharacter("evilness", "wizard");
         System.out.println(evil.getcharacter_Name());
+        evil.rollStats();
         evil.getStats();
 
 
