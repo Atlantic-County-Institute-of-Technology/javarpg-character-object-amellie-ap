ba150c2c419fd39a04a27f4c5410d8611bd908e3
ariz9258 <127510476+amellie-ap@users.noreply.github.com>
2026-02-05 12:48:58 -0500

    Main.java
    Finished, just need to add comments.

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Character evil = new Character();
         evil.createCharacter("evilness", "wizard");
-        System.out.println(evil.getcharacter_Name());
         evil.rollStats();
-        evil.getStats();
+        System.out.println(evil);
+
+
 
 
     }
