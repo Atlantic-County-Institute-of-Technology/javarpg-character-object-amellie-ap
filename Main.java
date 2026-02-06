91a8b57cd7b641f3ff37a6f2bd4985d8892152cd
ariz9258 <127510476+amellie-ap@users.noreply.github.com>
2026-02-05 22:23:49 -0500

    Main.java
    Completed version.

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
+// Main.java
+// @author: A_Ariz
+// last update: 2.5.2026
+ 
+//Our public class. 
 public class Main {
     public static void main(String[] args) {
-        Character evil = new Character();
-        evil.createCharacter("evilness", "wizard");
-        evil.rollStats();
-        System.out.println(evil);
-
-
-
+        //Creates new Character object under the name "Chara"
+        Character Chara = new Character();
+        //Creates new character and assigns the character class as wizard. 
+        Chara.createCharacter("John Java", "wizard");
+        //Rolls the character's stats by calling rollStats. 
+        Chara.rollStats();
+        System.out.println(Chara);
 
     }
 }
